44cf4c28bf23dc0881838331d19c882fc9036162
ishapaliwal <ishasona97@gmail.com>
2025-04-10 17:34:11 -0400

    Backend support integration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "postgresql://postgres:kVMFNftfBsmVSrPTuUQhNOSeEqcnZOGy@ballast.proxy.rlwy.net:28517/railway",
 });
 
 API.interceptors.request.use((config) => {
